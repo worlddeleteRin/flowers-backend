e59a6b95be5ea7ff7cd01ff2d7e47051f8cd3e3d
worlddeleteRin <worlddelete0@gmail.com>
2022-02-11 20:25:18 +0300

    typo

--- main_app/apps/site/models.py
+++ main_app/apps/site/models.py
@@ -9,12 +9,52 @@ from config import settings
 
 # site models changes here
 
+class SocialContact(BaseModel):
+    value: str = ""
+    label: str = ""
+    link: str = ""
+
+    @staticmethod
+    def get_defaults() -> list:
+        telegram_link = "https://fast-code.ru/"
+        viber_link = "https://fast-code.ru/"
+        whatsapp_link = "https://fast-code.ru/"
+        vk_link = "https://fast-code.ru/"
+        inst_link = "https://fast-code.ru/"
+        telegram = SocialContact(
+            value = "telegram",
+            label = "Telegram",
+            link = telegram_link
+        )
+        viber = SocialContact(
+            value = "viber",
+            label = "Viber",
+            link = viber_link
+        )
+        whatsapp = SocialContact(
+            value = "whatsapp",
+            label = "Whatsapp",
+            link = whatsapp_link
+        )
+        vk = SocialContact(
+            value = "vk",
+            label = "Vk",
+            link = vk_link
+        )
+        inst = SocialContact(
+            value = "inst",
+            label = "Instagram",
+            link = inst_link
+        )
+        return [telegram, viber, whatsapp, vk, inst]
+
 class CommonInfo(BaseModel):
     location_address: str = ""
     delivery_phone: str = ""
     delivery_phone_display: str = ""
     main_logo_link: str = ""
-    map_delivery_locaiton_link: str = ""
+    map_delivery_locaition_link: str = ""
+    socials: List[SocialContact] = []
 
     @staticmethod
     def get_default():
@@ -23,7 +63,8 @@ class CommonInfo(BaseModel):
             delivery_phone = "+79781111111",
             delivery_phone_display = "7 978 111 11 11",
             main_logo_link = settings.base_static_url + "logo_variant.png",
-            map_delivery_location_link = "https://yandex.ru/map-widget/v1/?um=constructor%3A9b116676061cfe4fdf22efc726567c5f21c243f18367e2b8a207accdae7e4786&amp;source=constructor"
+            map_delivery_location_link = "https://yandex.ru/map-widget/v1/?um=constructor%3A9b116676061cfe4fdf22efc726567c5f21c243f18367e2b8a207accdae7e4786&amp;source=constructor",
+            socials = SocialContact.get_defaults()
         )
         return info
 
